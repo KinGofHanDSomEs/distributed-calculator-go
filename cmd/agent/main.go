893875dae81cc5c458c0119a7f5fdbc4846f62c2
roman <romanf228@mail.ru>
2025-03-10 18:35:26 +0500

    adding run server with 2 terminal

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -4,16 +4,11 @@ import (
 	"sync"
 
 	"github.com/kingofhandsomes/distributed_calculator_go/internal/transport/agent"
-	"github.com/kingofhandsomes/distributed_calculator_go/internal/transport/orchestrator"
 )
 
 func main() {
-	wg := sync.WaitGroup{}
-	wg.Add(2)
-	go func() {
-		defer wg.Done()
-		orchestrator.NewOrchestrator().Run()
-	}()
+	var wg sync.WaitGroup
+	wg.Add(1)
 	go func() {
 		defer wg.Done()
 		agent.NewAgent().Run()
